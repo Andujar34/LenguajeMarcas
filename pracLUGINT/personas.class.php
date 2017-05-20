80b65fd253fa313d4948430bc4f33c933eb3ac54
andujar <andujar34>
2017-05-20 12:56:47 +0200

    p: mistery

--- pracLUGINT/personas.class.php
+++ pracLUGINT/personas.class.php
@@ -19,17 +19,21 @@ function giveList()
     $this->xpath = new DOMXPath($this->dom);
 
     foreach ($this->xpath->query("//voto") as $nodo) {
-
+        
+     
         $ciudad=$this->locateIp($nodo->childNodes[3]->nodeValue);
+          
         if(empty($datos[$ciudad])){
             $puntuacion=$nodo->childNodes[1]->nodeValue;
             $datos[$ciudad] =array("votantes"=>"1","votos"=>$puntuacion);
+           
            
           }else{ 
             $votantes =((int)$datos[$ciudad]['votantes'])+1;
             $puntuacion=((int)$nodo->childNodes[1]->nodeValue)+((int)$datos[$ciudad]['votos']);
            $datos[$ciudad] =array("votantes"=>$votantes,"votos"=>$puntuacion);
-          }    
+          }  
+          
          
     }
  echo json_encode(array("datos"=>$datos,"status"=>"ok","mensaje"=>"consulta exitosa"));
